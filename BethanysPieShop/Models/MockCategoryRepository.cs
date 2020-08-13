c76fca507a1dba12f3de15df33070ce151f2b43f
Alex Jordany Parrales López <alexjordany@outlook.com>
2020-08-12 19:07:42 -0600

    Creating the model and the repository

---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category {CategoryId=1,CategoryName="Fruit pies",Description="All-fruity pies"},
+                new Category {CategoryId=2,CategoryName="Cheese cake",Description="Cheesy aññ the way"},
+                new Category {CategoryId=1,CategoryName="Seasonal pies",Description="Get in to the mood for a seasonal pie"}
+            };
+    }
+}
