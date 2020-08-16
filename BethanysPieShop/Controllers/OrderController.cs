dcc39bdd6fa3f78fd3a9ed4885f9c1b074d676e5
Alex Jordany Parrales López <alexjordany@outlook.com>
2020-08-15 20:01:34 -0600

    Creating an order form, using model binding, adding validation to the form

---
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var Items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = Items;
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies firts");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
+            return View();
+        }
+    }
+}
